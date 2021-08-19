c7787a0d69587ef7ce7850951c0a961e8e45be08
khewdev <85968137+khewdev@users.noreply.github.com>
2021-08-19 22:04:41 +0800

    Time: 100 ms (80.61%), Space: 43.3 MB (76.76%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let i = 0;
+    let j = 0;
+    let max = 0;
+    
+    let set = new Set();
+    
+    while(j < s.length) {
+        if(!set.has(s[j])) {
+            set.add(s[j++]);
+            max = Math.max(set.size, max);
+        } else {
+            set.delete(s[i++]);
+        }
+    }
+    
+    return max;
+};
