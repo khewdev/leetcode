8c5e0a2b67e7cfa1f22028621f77b2538d470e43
khewdev <85968137+khewdev@users.noreply.github.com>
2022-07-06 03:50:52 +0800

    Time: 827 ms (61.42%), Space: 80.8 MB (40.42%) - LeetHub

--- 146-lru-cache/146-lru-cache.go
+++ 146-lru-cache/146-lru-cache.go
@@ -1,52 +1,57 @@
+package main
+
+import "fmt"
+
 type LRUCache struct {
-    Head *Node
-    Tail *Node
-    HT map[int]*Node
-    Cap int
+	Head *Node
+	Tail *Node
+	Map  map[int]*Node
+	Cap  int
 }
 
 type Node struct {
-    Key int
-    Val int
-    Prev *Node
-    Next *Node
+	Key  int
+	Val  int
+	Prev *Node
+	Next *Node
 }
 
 func Constructor(capacity int) LRUCache {
-    return LRUCache{HT: make(map[int]*Node), Cap: capacity}
+	return LRUCache{Map: make(map[int]*Node), Cap: capacity}
 }
 
 func (this *LRUCache) Get(key int) int {
-    node, ok := this.HT[key]
-    if ok {
-        this.Remove(node)
-        this.Add(node)
-        return node.Val
-    }
-    return -1
+	node, ok := this.Map[key]
+	if ok {
+		this.Remove(node)
+		this.Add(node)
+		return node.Val
+	}
+	return -1
 }
 
-func (this *LRUCache) Put(key int, value int)  {
-    node, ok := this.HT[key]
-    if ok {
-        node.Val = value
-        this.Remove(node)
-        this.Add(node)
-        return
-    } else {
-        node = &Node{Key: key, Val: value}
-        this.HT[key] = node
-        this.Add(node)
-    }
-    if len(this.HT) > this.Cap {
-        delete(this.HT, this.Tail.Key)
-        this.Remove(this.Tail)
-    }
+func (this *LRUCache) Put(key int, value int) {
+	node, ok := this.Map[key]
+
+	if ok {
+		this.Remove(node)
+		node.Val = value
+		this.Add(node)
+	} else {
+		if len(this.Map) == this.Cap {
+			delete(this.Map, this.Tail.Key)
+			this.Remove(this.Tail)
+		}
+		node = &Node{Key: key, Val: value}
+		this.Map[key] = node
+		this.Add(node)
+	}
 }
 
 func (this *LRUCache) Add(node *Node) {
     node.Prev = nil
     node.Next = this.Head
+    
     if this.Head != nil {
         this.Head.Prev = node
     }
@@ -54,9 +59,24 @@ func (this *LRUCache) Add(node *Node) {
     if this.Tail == nil {
         this.Tail = node
     }
+    
+// 	headNext := node.Next
+// 	this.Head.Next = node
+
+// 	node.Prev = this.Head
+// 	node.Next = headNext
+
+// 	headNext.Prev = node
+
 }
 
 func (this *LRUCache) Remove(node *Node) {
+// 	nextNode := node.Next
+// 	prevNode := node.Prev
+
+// 	nextNode.Prev = prevNode
+// 	nextNode.Next = nextNode
+    
     if node != this.Head {
         node.Prev.Next = node.Next
     } else {
@@ -68,3 +88,10 @@ func (this *LRUCache) Remove(node *Node) {
         this.Tail = node.Prev
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
