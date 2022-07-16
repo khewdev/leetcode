5dc28a02bd81a30db12ab6b19bb529763caf9f67
khewdev <85968137+khewdev@users.noreply.github.com>
2022-07-16 16:26:28 +0800

    Time: 70 ms (9.73%), Space: 8.8 MB (8.94%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -4,19 +4,22 @@ func productExceptSelf(nums []int) []int {
     left := 1
     
     for i := 0; i < size; i++ {
-        if i > 0 {
-            left *= nums[i - 1]
-        }
+        // if i > 0 {
+        //     left *= nums[i - 1]
+        // }
         res[i] = left
+        left *= nums[i]
     }
     
     right := 1
     for i := size - 1; i >= 0; i-- {
-        if i < size - 1 {
-            right *= nums[i + 1]
-        }
-        
         res[i] *= right
+        right *= nums[i]
+
+        // if i < size - 1 {
+        //     right *= nums[i + 1]
+        // }
+        
     }
     
     return res
