64a9f3edb0789ea689f4cf7c8bdbf1ec3cf7845e
khewdev <85968137+khewdev@users.noreply.github.com>
2022-07-20 17:47:46 +0800

    Time: 815 ms (67.44%), Space: 80.9 MB (38.68%) - LeetHub

--- 146-lru-cache/146-lru-cache.go
+++ 146-lru-cache/146-lru-cache.go
@@ -1,72 +1,69 @@
 type LRUCache struct {
 	head, tail *Node
-	keys       map[int]*Node
-	capacity   int
+	Keys       map[int]*Node
+	Cap        int
 }
 
 type Node struct {
-	key, val   int
-	prev, next *Node
+	Key, Val   int
+	Prev, Next *Node
 }
 
 func Constructor(capacity int) LRUCache {
-	return LRUCache{keys: make(map[int]*Node), capacity: capacity}
+	return LRUCache{Keys: make(map[int]*Node), Cap: capacity}
 }
 
 func (this *LRUCache) Get(key int) int {
-	if node, ok := this.keys[key]; ok {
+	if node, ok := this.Keys[key]; ok {
 		this.Remove(node)
 		this.Add(node)
-		return node.val
+		return node.Val
 	}
 	return -1
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	if node, ok := this.keys[key]; ok {
-		node.val = value
+	if node, ok := this.Keys[key]; ok {
+		node.Val = value
 		this.Remove(node)
 		this.Add(node)
 		return
 	} else {
-		node = &Node{key: key, val: value}
-		this.keys[key] = node
+		node = &Node{Key: key, Val: value}
+		this.Keys[key] = node
 		this.Add(node)
 	}
-	if len(this.keys) > this.capacity {
-		delete(this.keys, this.tail.key)
+	if len(this.Keys) > this.Cap {
+		delete(this.Keys, this.tail.Key)
 		this.Remove(this.tail)
 	}
 }
 
 func (this *LRUCache) Add(node *Node) {
-	node.prev = nil
-	node.next = this.head
+	node.Prev = nil
+	node.Next = this.head
 	if this.head != nil {
-		this.head.prev = node
+		this.head.Prev = node
 	}
 	this.head = node
 	if this.tail == nil {
 		this.tail = node
-		this.tail.next = nil
+		this.tail.Next = nil
 	}
 }
 
 func (this *LRUCache) Remove(node *Node) {
 	if node == this.head {
-		this.head = node.next
-		if node.next != nil {
-			node.next.prev = nil
-		}
-		node.next = nil
+		this.head = node.Next
+		node.Next = nil
 		return
 	}
 	if node == this.tail {
-		this.tail = node.prev
-		node.prev.next = nil
-		node.prev = nil
+		this.tail = node.Prev
+		node.Prev.Next = nil
+		node.Prev = nil
 		return
 	}
-	node.prev.next = node.next
-	node.next.prev = node.prev
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
 }
