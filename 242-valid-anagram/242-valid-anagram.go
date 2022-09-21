476e5c217813b6680d0fb6ad95a36c050434f7f0
khewdev <85968137+khewdev@users.noreply.github.com>
2022-09-22 06:40:36 +0800

    Time: 53 ms (5.27%), Space: 6.7 MB (5.08%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.go
@@ -0,0 +1,9 @@
+func isAnagram(s string, t string) bool {
+    first := strings.Split(s, "")
+    second := strings.Split(t, "")
+
+    sort.Strings(first)
+    sort.Strings(second)
+    
+    return strings.Join(first, "") == strings.Join(second, "")
+}
