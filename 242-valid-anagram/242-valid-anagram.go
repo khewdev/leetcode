35de5bd9522f9c91aa308a6b261c65efe992fe5e
khewdev <85968137+khewdev@users.noreply.github.com>
2022-09-22 06:50:24 +0800

    Time: 43 ms (5.27%), Space: 6.7 MB (5.08%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.go
+++ 242-valid-anagram/242-valid-anagram.go
@@ -1,24 +1,9 @@
 func isAnagram(s string, t string) bool {
-if len(s) != len(t) {
-        return false
-    }
-    
-    m := make(map[rune]int)
-    
-    for _, c := range s {
-        m[c]++
-    }
-    
-    for _, c := range t {
-        if _, ok := m[c]; !ok {
-            return false
-        } else {
-            m[c] --
-            if m[c] < 0 {
-                return false
-            }
-        }
-    }
-    
-    return true
+	first := strings.Split(s, "")
+	second := strings.Split(t, "")
+
+	sort.Strings(first)
+	sort.Strings(second)
+
+	return strings.Join(first, "") == strings.Join(second, "")
 }
