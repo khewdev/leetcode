30c11ba473342d84d29906e16d7d9ab58dc877d3
khewdev <85968137+khewdev@users.noreply.github.com>
2022-09-22 08:56:37 +0800

    Time: 43 ms (51.80%), Space: 9.3 MB (15.31%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.go
+++ 49-group-anagrams/49-group-anagrams.go
@@ -2,12 +2,11 @@ type CharCount [26]int
 
 func groupAnagrams(strs []string) [][]string {
     hash := make(map[CharCount][]string)
-    
     for _, word := range strs {
         charCount := CharCount{}
         
         for i := 0; i < len(word); i++ {
-            charCount[word[i] - 'a'] ++
+            charCount[word[i] - 'a']++
         }
         
         hash[charCount] = append(hash[charCount], word)
