358acc602733367ce9c67d43a78d1a7e657c24da
khewdev <85968137+khewdev@users.noreply.github.com>
2022-09-30 18:51:52 +0800

    Time: 3 ms (67.45%), Space: 2.5 MB (100.00%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.go
+++ 206-reverse-linked-list/206-reverse-linked-list.go
@@ -5,9 +5,19 @@
  *     Next *ListNode
  * }
  */
-func reverseList(head *ListNode)  (prev *ListNode) {
-    for head != nil {
-		head.Next, prev, head = prev, head, head.Next
+func reverseList(head *ListNode) *ListNode {
+    curr := head
+	var prev *ListNode = nil
+	var next *ListNode = nil
+
+	for curr != nil {
+		next = curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
 	}
-	return
+
+	head = prev
+
+	return head
 }
