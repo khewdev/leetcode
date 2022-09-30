a1ad992dc0035b5c2fc1b8134f0f6ff6759ed70c
khewdev <85968137+khewdev@users.noreply.github.com>
2022-10-01 03:02:33 +0800

    Time: 19 ms (58.95%), Space: 3.6 MB (58.60%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.go
@@ -0,0 +1,12 @@
+func lengthOfLIS(nums []int) int {
+    dp := []int{}
+	for _, num := range nums {
+		i := sort.SearchInts(dp, num)
+		if i == len(dp) {
+			dp = append(dp, num)
+		} else {
+			dp[i] = num
+		}
+	}
+	return len(dp)
+}
