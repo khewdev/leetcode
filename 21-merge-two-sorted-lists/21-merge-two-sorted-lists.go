8b754e4a20aa7967baf1956ceaf6cd9739a91e06
khewdev <85968137+khewdev@users.noreply.github.com>
2022-10-03 01:02:58 +0800

    Time: 4 ms (58.45%), Space: 2.5 MB (52.58%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.go
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.go
@@ -7,25 +7,25 @@
  */
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    var resultNode = new(ListNode)
-    var tempNode = resultNode
+    dummy := new(ListNode)
+    temp := dummy
     
     for l1 != nil && l2 != nil {
         if l1.Val < l2.Val {
-            tempNode.Next = l1
+            temp.Next = l1
             l1 = l1.Next
         } else {
-            tempNode.Next = l2
+            temp.Next = l2
             l2 = l2.Next
         }
-        tempNode = tempNode.Next
+        temp = temp.Next
     }
     
     if l1 != nil {
-        tempNode.Next = l1
+        temp.Next = l1
     } else {
-        tempNode.Next = l2
+        temp.Next = l2
     }
     
-    return resultNode.Next
+    return dummy.Next
 }
