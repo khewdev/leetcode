a5ee6e5a553967fe80893906f8678bece1fa1a83
khewdev <85968137+khewdev@users.noreply.github.com>
2022-10-06 02:47:02 +0800

    Time: 133 ms (63.02%), Space: 9.1 MB (47.38%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.go
+++ 11-container-with-most-water/11-container-with-most-water.go
@@ -1,24 +1,12 @@
-func max(x int, y int) int {
-    if x < y {
-        return y
-    }
-    return x
-}
-
-func min(x int, y int) int {
-    if x < y {
-        return x
-    }
-    return y
-}
+import "math"
 
 func maxArea(height []int) int {
     res := 0
     l, r := 0, len(height) - 1
     
     for l < r {
-        area := (r - l) * min(height[l] ,height[r])
-        res = max(res, area)
+        area := (r - l) * int(math.Min(float64(height[l]), float64(height[r])))
+        res = int(math.Max(float64(res), float64(area)))
         
         if height[l] < height[r] {
             l += 1
