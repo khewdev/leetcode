812fe1879957c97e5b14e7e1e551973bf38eed2c
khewdev <85968137+khewdev@users.noreply.github.com>
2022-10-11 00:10:09 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (32.76%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    dummy := &ListNode{0, head}
+    
+    left := dummy
+    right := head
+    
+    for n > 0 && right != nil {
+        right = right.Next
+        n--
+    }
+    
+    for right != nil {
+        left = left.Next
+        right = right.Next
+    }
+    
+    left.Next = left.Next.Next
+    
+    return dummy.Next
+}
