0365419669038b0039ba2bab64cc07037b02000f
khewdev <85968137+khewdev@users.noreply.github.com>
2022-10-29 23:11:04 +0800

    Time: 16 ms (46.25%), Space: 2.9 MB (29.82%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.go
+++ 0013-roman-to-integer/0013-roman-to-integer.go
@@ -1,27 +1,27 @@
 var roman = map[string]int{
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
 }
 
 func romanToInt(s string) int {
-    if s == "" {
-        return 0
-    }
-    
-    res := 0
-    
-    for i := 0; i < len(s); i++ {
-        if i + 1 < len(s) && roman[string(s[i])] < roman[string(s[i+1])] {
-            res -= roman[string(s[i])]
-        } else {
-            res += roman[string(s[i])]
-        }
-    }
-    
-    return res
+	if s == "" {
+		return 0
+	}
+	num, lastint, total := 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		char := s[len(s)-(i+1) : len(s)-i]
+		num = roman[char]
+		if num < lastint {
+			total = total - num
+		} else {
+			total = total + num
+		}
+		lastint = num
+	}
+	return total
 }
