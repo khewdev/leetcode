4181fc18e0dc78d9933ceeec4c9e046c104ff289
khewdev <85968137+khewdev@users.noreply.github.com>
2023-05-20 19:14:17 +0800

    Time: 124 ms (33.97%), Space: 7.5 MB (71.29%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.go
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.go
@@ -9,8 +9,14 @@ func replaceElements(arr []int) []int {
     j, temp := -1, 0
     
     for i := len(arr) - 1; i >= 0; i-- {
+        // set current index value to temp
         temp = arr[i]
+        
+        // in the first iteration we set last element to -1
+        // in following iterations we will set j to current index, which we got the largest value of current index all the way to right side from previous iterations
         arr[i] = j
+        
+        // set largest value of current index all the way to right side to j
         j = max(j, temp)
     }
     
