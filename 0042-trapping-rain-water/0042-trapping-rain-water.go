d655914b7e431eba23bc959e8c4e5fb0efd8a3a1
khewdev <85968137+khewdev@users.noreply.github.com>
2023-07-25 18:36:59 +0800

    Time: 12 ms (61.92%), Space: 5.3 MB (52.33%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -0,0 +1,26 @@
+import "math"
+
+func trap(height []int) int {
+    if height == nil {
+        return 0
+    }
+    
+    l, r := 0, len(height) - 1
+    leftMax, rightMax := height[l], height[r]
+    res := 0
+    
+    for l < r {
+        if leftMax < rightMax {
+            l++
+            leftMax = int(math.Max(float64(leftMax), float64(height[l])))
+            res += leftMax - height[l]
+        } else {
+            r--
+            rightMax = int(math.Max(float64(rightMax), float64(height[r])))
+            res += rightMax - height[r]
+        }
+    }
+    
+    return res
+    
+}
