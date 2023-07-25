7fd19beb8c238e1323b4761cca70f1998a2e0c14
khewdev <85968137+khewdev@users.noreply.github.com>
2023-07-26 00:39:48 +0800

    Time: 13 ms (50.98%), Space: 5.4 MB (52.33%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.go
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -6,22 +6,40 @@ func max(a, b int) int {
 }
 
 func trap(height []int) int {
-    if height == nil {
+    if height == nil || len(height) < 3 {
         return 0
     }
     
+    // left pointer and right pointer
     l, r := 0, len(height) - 1
+    
+    // initializing left max and right max
     leftMax, rightMax := height[l], height[r]
+    
+    // result
     res := 0
     
+    // while left pointer < right pointer
     for l < r {
         if leftMax < rightMax {
-            l++
-            leftMax = max(leftMax, height[l])
+            l++ // increment left pointer by 1 (move right 1 step)
+            
+            // update leftMax with the max of current leftMax and 
+            // the newly shifted left pointer's height
+            leftMax = max(leftMax, height[l]) 
+            
+            // newly updated leftMax - newly shifted left pointer height works because
+            // newly shifted left pointer height must be lower than leftMax
+            // in order to trap water
             res += leftMax - height[l]
         } else {
-            r--
+            r-- // decrement right pointer by 1 (move left 1 step)
+            
+            // update rightMax with the max of current rightMax and 
+            // the newly shifted right pointer's height
             rightMax = max(rightMax, height[r])
+
+            // newly updated rightMax - newly shifted right pointer height
             res += rightMax - height[r]
         }
     }
