ef6d0376aa7cac33f344f5fa8e33e3cad8da57fb
khewdev <85968137+khewdev@users.noreply.github.com>
2023-07-27 01:47:41 +0800

    Time: 134 ms (46.66%), Space: 8.7 MB (27.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,19 +1,18 @@
 func max(a int, b int) int {
     if a > b {
         return a
-    }
+    } 
     return b
 }
 
 func maxProfit(prices []int) int {
-    l, r := 0, 1 // left pointer=buy, right pointer=sell
+    l, r := 0, 1
     res := 0
     
     for r < len(prices) {
-        if prices[l] < prices[r] {
+        if prices[r] > prices[l] {
             res = max(res, prices[r] - prices[l])
         } else {
-            // l++
             l = r
         }
         r++
