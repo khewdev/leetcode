ebfb5b07d7282cf95a86a6dd67c1b88f144b05ed
khewdev <85968137+khewdev@users.noreply.github.com>
2023-07-28 01:07:46 +0800

    Time: 93 ms (98.53%), Space: 16.4 MB (46.61%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,11 +3,14 @@ class Solution:
         count = {}
         res = 0
         l = 0
+        maxFrequency = 0
         
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
             
-            while (r - l + 1) - max(count.values()) > k:
+            maxFrequency = max(maxFrequency, count[s[r]])
+            
+            while (r - l + 1) - maxFrequency > k:
                 count[s[l]] -= 1
                 l += 1
             
